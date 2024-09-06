4c58e1c5c01ce95bbef9df3c885c59a317cf9b12
s711183122 <s711183122@gm.ntpu.edu.tw>
2024-09-06 23:33:57 +0800

    0906 1.2.0

--- push_git.ps1
+++ push_git.ps1
@@ -1,7 +1,7 @@
 cd ./dist
 git init
 git add .
-git commit -m "0825 1.1.0"
+git commit -m "0905 1.2.0"
 git branch -M main
 git remote add origin https://github.com/nishinokm/TanZ_btn_web.git
 git push -u origin main -f
